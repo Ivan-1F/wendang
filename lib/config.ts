1b2f32520672bd900401ca366a3707066bef9886
Ivan1F <wyf0762@foxmail.com>
2026-01-27 21:07:00 +0800

    added iconLoader to schema

--- lib/config.ts
+++ lib/config.ts
@@ -1,9 +1,16 @@
 import type { ReactNode } from 'react';
 import z from 'zod';
 
+const reactNodeSchema = z.custom<ReactNode>();
+
+const iconLoaderSchema = z.function({
+  input: [z.string()],
+  output: reactNodeSchema,
+});
+
 const sectionSchema = z.object({
   title: z.string(),
-  icon: z.custom<ReactNode>().optional(),
+  icon: reactNodeSchema.optional(),
   base: z.string().optional(),
   get children() {
     return pageSchema.array();
@@ -14,7 +21,7 @@ const pageSchema = z.union([z.string(), sectionSchema]);
 
 const groupSchema = z.object({
   title: z.string(),
-  icon: z.custom<ReactNode>().optional(),
+  icon: reactNodeSchema.optional(),
   link: z.string(),
   external: z.boolean().default(false),
   align: z.enum(['leading', 'trailing']).default('leading'),
@@ -37,6 +44,7 @@ const singleGroupConfigSchema = z
 
 const docsConfigSchema = z.object({
   group: z.union([groupConfigSchema, singleGroupConfigSchema]),
+  iconLoader: iconLoaderSchema.optional(),
 });
 
 export type DocsConfig = z.infer<typeof docsConfigSchema>;
