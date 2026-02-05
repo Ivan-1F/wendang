a05d0400096bf95c82b8d1730dfde44c8715c0b8
Ivan1F <wyf0762@foxmail.com>
2026-02-05 12:50:31 +0800

    removed example docs

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,8 @@
 import { setRequestLocale } from 'next-intl/server';
-import { ComponentExample } from "@/components/component-example";
 
 export default async function Page({ params }: PageProps<'/[locale]'>) {
   const { locale } = await params;
   setRequestLocale(locale);
 
-  return <ComponentExample />;
+  return <div>Go to /docs to view the document</div>;
 }
